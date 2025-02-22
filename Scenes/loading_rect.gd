4ab481035b7e566f634894ce507863f3c5830c9f
Shadib Shahriyar Hoque <160192592+shadibhoque@users.noreply.github.com>
2025-02-22 03:19:43 -0600

    Add files via upload

---
+++ Scenes/loading_rect.gd
@@ -0,0 +1,19 @@
+extends ColorRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rect_min_size = Vector2(1920, 1080)  # Set this to match your game resolution
+
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
