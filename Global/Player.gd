09a466404c79f91521516c9360cd6e4d5727f563
Shadib Shahriyar Hoque <shadib.shahriyar@gmail.com>
2025-02-21 19:17:37 -0600

    fixed obstacles

--- Global/Player.gd
+++ Global/Player.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
-const SPEED = 200
+const NORMAL_SPEED = 200
+var slow_speed = 75
+var speed = NORMAL_SPEED  # Current speed
+var slow_obstacle_count = 0 
+
 var velocity = Vector2()
 
 func _physics_process(delta):
@@ -8,8 +12,22 @@ func _physics_process(delta):
 	# Input handling
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	velocity = velocity.normalized() * SPEED
+	velocity = velocity.normalized() * NORMAL_SPEED
 	move_and_slide(velocity)
+
+
+# Called when entering a slow obstacle area
+func _on_Hitbox_area_entered(area):
+	if area.is_in_group("slow_obstacles"):
+		slow_obstacle_count += 1
+		speed = slow_speed  # Apply slow speed
+
+# Called when exiting a slow obstacle area
+func _on_Hitbox_area_exited(area):
+	if area.is_in_group("slow_obstacles"):
+		slow_obstacle_count -= 1
+		if slow_obstacle_count == 0:
+			speed = NORMAL_SPEED  # Restore normal speed
 
 
 
