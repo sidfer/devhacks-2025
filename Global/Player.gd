76eb11517fabf0717b0211c209c27a4a9611c8bb
ArqamAdiyan <75317921+ArqamAdiyan@users.noreply.github.com>
2025-02-22 00:03:10 -0600

    added sick animatioons

--- Global/Player.gd
+++ Global/Player.gd
@@ -4,13 +4,14 @@ const NORMAL_SPEED = 200
 var slow_speed = 75
 var speed = NORMAL_SPEED  # Current speed
 var slow_obstacle_count = 0 
+var player_direction= "Up"
 
 var velocity = Vector2()
 var can_move = true  # Movement control flag
 
 func _ready():
 	add_to_group("player")
-	
+
 	$Hitbox.connect("area_entered", self, "_on_Hitbox_area_entered")
 	$Hitbox.connect("area_exited", self, "_on_Hitbox_area_exited")
 
@@ -19,11 +20,43 @@ func _ready():
 func _physics_process(delta):
 	if !can_move:
 		return
-		
-	velocity = Vector2()
+	
+	# Get horizontal input strength (left and right)
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	# Get vertical input strength (up and down)
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	# Normalize the velocity so diagonal movement isn't faster
 	velocity = velocity.normalized() * speed
+	
+	# Handle animations based on movement
+	if velocity.length() > 0:  # Player is moving
+		if velocity.x > 0:  # Moving right
+			$SpriteRight.play("RightWalk")
+			player_direction= "Right"
+		elif velocity.x < 0:  # Moving left
+			$SpriteRight.play("LeftWalk")
+			player_direction= "Left"
+		elif velocity.y > 0:  # Moving down
+			$SpriteRight.play("DownWalk")
+			player_direction= "Down"
+		elif velocity.y < 0:  # Moving up
+			$SpriteRight.play("UpWalk")
+			player_direction= "Up"
+	else:  # Player is idle
+		# Play the idle animation based on the last direction
+		if player_direction == "Right":
+			$SpriteRight.play("IdleRight")
+		elif player_direction == "Left":    
+			$SpriteRight.play("IdleLeft")
+		elif player_direction == "Down":
+			$SpriteRight.play("IdleDown")
+		elif player_direction == "Up":
+			$SpriteRight.play("IdleUp")
+		
+	
+	# Move the player
 	move_and_slide(velocity)
 
 
